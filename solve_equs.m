98464153341442196659f5305cd77a361c318157
hjc2000 <huangjincanqqmail@qq.com>
2024-03-16 21:45:51 +0800

    function ret = solve_equs(equs, unknown_syms)

--- solve_equs.m
+++ solve_equs.m
@@ -2,16 +2,10 @@
     利用高斯消元法解方程组
     
     参数：
-        - equs: 方程组
-        - known_syms：已知变量。在解方程的角度，像系统的输入变量、输出变量、
-		  已知的常数等都是已知变量。因为这些是不被消元的。
+        - equs 方程组
+        - unknown_syms 未知变量的集合，要被消元消掉的。
 %}
-function ret = solve_equs(equs, known_syms)
-	% 获取所有符号变量
-	all_syms = symvar(equs);
-	% 移除已知变量，得到未知变量的集合
-	unknown_syms = remove_syms(all_syms, known_syms);
-	
+function ret = solve_equs(equs, unknown_syms)
 	% 在循环里消元，不断消除未知变量
 	index = 1;
 	while true
